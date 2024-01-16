9767500ef24adf02135564a1afb1374b079dedf3
Petros <p.gabrielyan@gmail.com>
2024-01-16 23:17:17 +0400

    added filters, signature

---
+++ AppStoreReviewManager.swift
@@ -0,0 +1,32 @@
+//
+//  AppStoreReviewManager.swift
+//  Scanner
+//
+//  Created by Petros Gabrielyan on 16.01.24.
+//
+
+import StoreKit
+
+enum AppStoreReviewManager {
+    static func requestReviewIfAppropriate() {
+        SKStoreReviewController.requestReviewInCurrentScene()
+    }
+    
+    static func requestReviewInAppStore() {
+        guard let writeReviewURL = URL(string: "https://apps.apple.com/app/id6458876381?action=write-review")
+            else { fatalError("Expected a valid URL") }
+        UIApplication.shared.open(writeReviewURL, options: [:])
+//        let url = URL(string: "")! //"?ls=1&mt=8&action=write-review"
+//        UIApplication.shared.open(url, options: [:])
+    }
+}
+
+extension SKStoreReviewController {
+    public static func requestReviewInCurrentScene() {
+        if let scene = UIApplication.shared.connectedScenes.first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene {
+            DispatchQueue.main.async {
+                requestReview(in: scene)
+            }
+        }
+    }
+}
--- Scanner/Screen/Camera/EditViewController.swift
+++ Scanner/Screen/Camera/EditViewController.swift
@@ -8,39 +8,120 @@
 import UIKit
 import CropViewController
 
-protocol ReadyImageDelegate: AnyObject {
-    func save(_ image: UIImage)
-}
-
 class EditViewController: UIViewController {
     
     @IBOutlet weak var editImg: UIImageView!
     @IBOutlet weak var convertButton: UIButton!
+    @IBOutlet weak var movableView: UIView!
+    @IBOutlet weak var movableImageView: UIImageView!
     @IBOutlet weak var filterCollectionView: UICollectionView!
+    @IBOutlet weak var mainBottomBar: UIView!
+    @IBOutlet weak var confirmationBottomBar: UIView!
     
+    var move = true
+    
+    let borderWidth: CGFloat = 1
+    let resizersWidth: CGFloat = 6
+    let rotatersWidth: CGFloat = 16
+
+    var deltaX: CGFloat = 0
+    var deltaY: CGFloat = 0
+    var diff: CGFloat = .pi
+    var prevLocation = CGPoint(x: 0, y: 0)
+    var identity = CGAffineTransform.identity
+    var identityForDoc = CGAffineTransform.identity
+    
+    var currentWidth: CGFloat = 0
+    var currentHeight: CGFloat = 0
     var collectionIsShown = false
     
-    var data = [String]()
+    var data = ["Original", "Vivid", "Vivid Warm", "Vivid Cool", "Dramatic", "Dramatic Warm", "Dramatic Cool", "Mono", "Silvertone", "Noir"]
+    var filterNames = ["none", "CIColorControls", "CIPhotoEffectChrome", "CIPhotoEffectFade", "CIPhotoEffectInstant", "CIPhotoEffectProcess", "CIPhotoEffectTonal", "CIPhotoEffectTransfer", "CISepiaTone", "CIPhotoEffectNoir"]
     
     var img: UIImage?
+    var originalImg = UIImage()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configUI()
         setupCollectionView()
+
+        movableImageView.layer.borderWidth = 1
+        movableImageView.isHidden = true
         
-        data = CIFilter.filterNames(inCategory: nil) // need 10 filters
+        currentWidth = movableImageView.bounds.width
+        currentHeight = movableImageView.bounds.height
+        
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
+        panGesture.maximumNumberOfTouches = 1
+        self.view.addGestureRecognizer(panGesture)
+        
+        let pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(handlePinch(_:)))
+        let rotationGesture = UIRotationGestureRecognizer(target: self, action: #selector(handleRotate(_:)))
+        
+        pinchGesture.delegate = self
+        rotationGesture.delegate = self
+        
+        self.movableView.addGestureRecognizer(pinchGesture)
+        self.movableView.addGestureRecognizer(rotationGesture)
+
+    }
+    
+    @objc func handlePan(_ sender: UIPanGestureRecognizer) {
+        let point = sender.location(in: self.view)
+        if sender.state == .began {
+            if !movableView.frame.contains(point) {
+                move = false
+                return
+            } else {
+                move = true
+            }
+        } else if move {
+            var center = movableView.center
+            let translation = sender.translation(in:self.view)
+                    center = CGPoint(x:center.x + translation.x,
+                                         y:center.y + translation.y)
+            movableView.center = center
+            sender.setTranslation(CGPoint.zero, in: sender.view)
+        }
+    }
+    
+    @objc func handlePinch(_ gesture: UIPinchGestureRecognizer) {
+        switch gesture.state {
+        case .began:
+            identity = movableView.transform
+            gesture.scale = 1
+        case .changed:
+            var t = identity.scaledBy(x: gesture.scale, y: gesture.scale)
+            movableView.transform = t
+            movableImageView.layer.borderWidth = borderWidth / sqrt(t.a*t.a+t.c*t.c) //gesture.scale
+        case .cancelled,.ended:
+            movableView.transform = identity.scaledBy(x: gesture.scale, y: gesture.scale)
+            gesture.scale = 1
+        default:
+            break
+        }
+        
+    }
+    
+    @objc func handleRotate(_ gesture: UIRotationGestureRecognizer) {
+        if gesture.state == .began, gesture.state == .ended, gesture.state == .cancelled {
+            gesture.rotation = 0
+        }
+        movableView.transform = movableView.transform.rotated(by: gesture.rotation)
     }
     
     private func configUI(){
-        if let img = img{
+        if let img = img {
             editImg.contentMode = .scaleAspectFit
             editImg.image = img
+            originalImg = img
         }
         convertButton.layer.borderWidth = 1
         convertButton.layer.borderColor = UIColor(red: 123/255, green: 155/255, blue: 130/255, alpha: 1).cgColor
         convertButton.cornerRadius = 14
+        
     }
     
     func setupCollectionView() {
@@ -48,8 +129,39 @@ class EditViewController: UIViewController {
         filterCollectionView.register(nib, forCellWithReuseIdentifier: FilterCell.id)
         filterCollectionView.delegate = self
         filterCollectionView.dataSource = self
+        
+        self.filterCollectionView.frame.origin.y = self.view.frame.height
     }
 
+    @IBAction func saveSignAction(_ sender: Any) {
+        hideConfirmationBar()
+        movableImageView.layer.borderWidth = 0
+        movableView.removeFromSuperview()
+        let t = movableView.transform
+        movableView.transform = .identity
+        let frame = self.view.convert(movableView.frame, to: editImg)
+        movableView.frame = frame
+        movableView.transform = t
+        editImg.addSubview(movableView)
+        for subview in movableView.subviews {
+            subview.isHidden = true
+        }
+        movableImageView.isHidden = false
+        let img = self.getImageFrom(view: self.editImg)
+        for subview in editImg.subviews {
+            subview.removeFromSuperview()
+        }
+        save(img ?? UIImage())
+    }
+    
+    @IBAction func cancelSignAction(_ sender: Any) {
+        hideConfirmationBar()
+        let vc = DrawSignatureViewController()
+        vc.delegate = self
+        vc.modalPresentationStyle = .overCurrentContext
+        self.present(vc, animated: true)
+    }
+    
     @IBAction func backButtonAction(_ sender: Any) {
         self.navigationController?.popViewController(animated: true)
     }
@@ -86,36 +198,85 @@ class EditViewController: UIViewController {
     }
     
     @IBAction func signatureAction(_ sender: Any) {
-        
+        let vc = DrawSignatureViewController()
+        vc.delegate = self
+        vc.modalPresentationStyle = .overCurrentContext
+        self.present(vc, animated: true)
     }
     
     @IBAction func filterAction(_ sender: Any) {
         if collectionIsShown {
-            UIView.animate(withDuration: 0.2) {
-                self.filterCollectionView.frame.origin.y = self.view.frame.height - self.filterCollectionView.frame.height
+            UIView.animate(withDuration: 0.3) {
+                self.filterCollectionView.frame.origin.y = self.view.frame.height
             }
         } else {
-            UIView.animate(withDuration: 0.2) {
+            UIView.animate(withDuration: 0.3) {
                 self.filterCollectionView.frame.origin.y = self.view.frame.height - 2 * self.filterCollectionView.frame.height
             }
         }
         self.collectionIsShown.toggle()
-//        self.img = simpleBlurFilterExample(inputImage: img!)
-//        self.editImg.image = self.img
     }
     
-    func simpleBlurFilterExample(inputImage: UIImage) -> UIImage {
-        // convert UIImage to CIImage
+    func setFilter(name: String, inputImage: UIImage) -> UIImage {
+        if name == "none" {
+            return inputImage
+        }
         let inputCIImage = CIImage(image: inputImage)!
         
-        // Create Blur CIFilter, and set the input image
-        let blurFilter = CIFilter(name: "CIColorControls")!
-        blurFilter.setValue(inputCIImage, forKey: kCIInputImageKey)
-        blurFilter.setValue(0, forKey: kCIInputSaturationKey)
+        let filter = CIFilter(name: name)!
+        filter.setDefaults()
+        filter.setValue(inputCIImage, forKey: kCIInputImageKey)
+        
+        if name == "CIColorControls" {
+            filter.setValue(1.5, forKey: kCIInputContrastKey)
+            filter.setValue(1.2, forKey: kCIInputSaturationKey)
+            filter.setValue(0.0, forKey: kCIInputBrightnessKey)
+        }
 
         // Get the filtered output image and return it
-        let outputImage = blurFilter.outputImage!
-        return UIImage(ciImage: outputImage)
+        let outputImage = filter.outputImage ?? inputCIImage
+        let resultImage = UIImage(ciImage: outputImage)
+        return resultImage
+    }
+    
+    func showConfirmationBar() {
+        UIView.animate(withDuration: 0.3) {
+            self.mainBottomBar.frame.origin.y = self.view.frame.height
+            
+            if self.filterCollectionView.frame.origin.y != self.view.frame.height {
+                self.filterCollectionView.frame.origin.y = self.view.frame.height
+            }
+            
+            self.confirmationBottomBar.frame.origin.y
+             = self.view.frame.height - 130
+        }
+    }
+    
+    func hideConfirmationBar() {
+        UIView.animate(withDuration: 0.3) {
+            self.mainBottomBar.frame.origin.y = self.view.frame.height - 130
+            self.confirmationBottomBar.frame.origin.y
+             = self.view.frame.height
+        }
+    }
+    
+    func save(_ image: UIImage) {
+        self.editImg.image = image
+        self.img = image
+        self.originalImg = image
+        filterCollectionView.reloadData()
+    }
+    
+    func getImageFrom(view: UIView) -> UIImage? {
+        UIGraphicsBeginImageContextWithOptions(view.bounds.size, false, 0.0)
+        
+        view.drawHierarchy(in: view.bounds, afterScreenUpdates: true)
+        
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        
+        UIGraphicsEndImageContext()
+        
+        return image
     }
 }
 
@@ -126,21 +287,19 @@ extension EditViewController: UICollectionViewDelegate, UICollectionViewDataSour
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FilterCell.id, for: indexPath) as! FilterCell
-        cell.imageView.backgroundColor = .red
+        cell.imageView.image = setFilter(name: filterNames[indexPath.row], inputImage: originalImg)
         cell.nameLabel.text = data[indexPath.row]
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let image = simpleBlurFilterExample(inputImage: img!)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FilterCell.id, for: indexPath) as! FilterCell
+        let image = cell.imageView.image
         editImg.image = image
-    }
-}
-
-extension EditViewController: ReadyImageDelegate {
-    func save(_ image: UIImage) {
-        self.editImg.image = image
-        self.img = image
+        img = image
+//        let image = setFilter(name: filterNames[indexPath.row], inputImage: img!)
+//        img = image
+//        editImg.image = image
     }
 }
 
@@ -152,5 +311,19 @@ extension EditViewController: CropViewControllerDelegate {
     
     func cropViewController(_ cropViewController: CropViewController, didFinishCancelled cancelled: Bool) {
         navigationController?.popViewController(animated: true)
+    }
+}
+
+extension EditViewController: SignatureDelegate {
+    func addSign(sign: UIImage) {
+        movableImageView.image = sign
+        movableImageView.isHidden = false
+        showConfirmationBar()
+    }
+}
+
+extension EditViewController: UIGestureRecognizerDelegate {
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
     }
 }
